063d3d72f7d19e287809466190d5175895455cc3
kyakit4 <>
2025-09-20 16:43:15 +0530

    Remove console.log statements from service worker to clear console errors

--- client/public/sw.js
+++ client/public/sw.js
@@ -15,7 +15,6 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
@@ -93,7 +92,6 @@ self.addEventListener('activate', (event) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
-            console.log('Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
